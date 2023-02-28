30c3b2d748a652bc79d35be477b9941cddfbbf7e
s21018 <s21018@std.it-cllege.ac.jp>
2023-02-28 18:36:00 +0900

    model Absences CerfiticateApplication 作成

--- app/Models/Absence.ts
+++ app/Models/Absence.ts
@@ -5,9 +5,24 @@ export default class Absence extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public student_name: string
+
+  @column()
+  public student_id: string
+
+  @column()
+  public kind: number
+
+  @column()
+  public reason: string
+
+  @column()
+  public start_date: DateTime
+
+  @column()
+  public end_date: DateTime
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
 }
